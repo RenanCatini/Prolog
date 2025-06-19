18629d3b8bcec231c2980a690fca688b0f06eb57
RenanCatini <renan.amaral@sou.unifal-mg.edu.br>
2025-06-19 20:08:47 -0300

    Implementação do trabalho de Prolog

--- trabalho/main.pl
+++ trabalho/main.pl
@@ -17,10 +17,72 @@ Em função desses parâmetros o programa deverá buscar (usando busca em largur
 profundidade ou heurística) e apresentar a solução do problema (se houver).
 */
 
+% Estado inicial = [M, C, 0, 0, esquerda]
+% Estado final = [0, 0, M, C, direita]
 
+/*
+    Busca em largura 
+    Código adaptado do busca em largura e em profundidade dado na
+    sessão "Material" do Moodle de Prolog.
+*/
+%-------------------------------------------------------------------------------------------
 
+% meta
+meta([0, 0, _, _, d]).
 
+atingemeta([_-E|_]) :- meta(E).
 
+ap([],X,X).
+ap([A|B],C,[A|D]) :- ap(B,C,D).
 
+busca([Caminho|_], Solucao, _) :- atingemeta(Caminho), !, Solucao = Caminho.
+busca([Caminho|Lista], Solucao, K) :- 
+   findall(UMAEXT, estende(Caminho, UMAEXT, K), EXT),
+   ap(Lista, EXT, Lista1),
+   busca(Lista1, Solucao, K).
 
+estende([OperacaoX-EstadoA|Caminho], [OperacaoY-EstadoB,OperacaoX-EstadoA|Caminho], K) :-
+   oper(OperacaoY, EstadoA, EstadoB, K),
+   not(member(OperacaoY-EstadoB, Caminho)).
 
+% ------------------------------------------------------------------------------------------
+
+% Checa se a posição atual é segura
+seguro([Me, Ce, Md, Cd, _]) :- 
+    (Me >= Ce ; Me == 0), 
+    (Md >= Cd ; Md == 0),
+    Me >= 0, Ce >= 0, Md >= 0, Cd >= 0.
+
+% Gera todas as possibilidades de barco.
+movimento(K, Me, Ce, M, C) :-
+    between(0, K, M),
+    between(0, K, C),
+    Total is M + C,
+    Total >= 1, Total =< K,
+    M =< Me, C =< Ce.
+
+% Operações de mudança de margem
+oper(Acao, [Me, Ce, Md, Cd, e], [Me2, Ce2, Md2, Cd2, d], K) :-
+    movimento(K, Me, Ce, M, C),
+    Me2 is Me - M, Ce2 is Ce - C,
+    Md2 is Md + M, Cd2 is Cd + C,
+    seguro([Me2, Ce2, Md2, Cd2, _]),
+    format(atom(Acao), 'Levar ~d M e ~d C para a margem direita.', [M, C]).
+
+oper(Acao, [Me, Ce, Md, Cd, d], [Me2, Ce2, Md2, Cd2, e], K) :-
+    movimento(K, Md, Cd, M, C),
+    Me2 is Me + M, Ce2 is Ce + C,
+    Md2 is Md - M, Cd2 is Cd - C,
+    seguro([Me2, Ce2, Md2, Cd2, _]),
+    format(atom(Acao), 'Levar ~d M e ~d C para a margem esquerda.', [M, C]).
+
+% Imprimir caminho
+imprimir_caminho([]).
+imprimir_caminho([Acao-Estado|R]) :-
+    imprimir_caminho(R),
+    write(Acao), write(' -> '), write(Estado), nl.
+
+% Função principal
+resolve(M, C, K) :- 
+    busca([[inicio-[M, C, 0, 0, e]]], Solucao, K),
+    imprimir_caminho(Solucao).
